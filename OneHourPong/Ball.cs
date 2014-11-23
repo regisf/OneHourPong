6da89674deda1fe2d57910f53cef10468496e26a
regisf <regisfloret@gmail.com>
2014-11-23 21:42:41 +0400

    Release

--- OneHourPong/Ball.cs
+++ OneHourPong/Ball.cs
@@ -4,11 +4,29 @@ using SFML.Window;
 
 namespace OneHourPong
 {
+	public class BallLostEventArgs : EventArgs 
+	{
+		public BallLostEventArgs(Paddle.Where w) {
+			Side = w;
+		}
+
+		public Paddle.Where Side {
+			get;
+			set;
+		}
+	}
+
+	public delegate void BallLostEventHandler(object sender, BallLostEventArgs e);
+
 	public class Ball
 	{
+		const uint SpeedIncrease = 1;
 		private CircleShape _ball;
 		private Vector2f _direction = new Vector2f();
 		private Vector2f _position = new Vector2f (395, 295);
+		private uint _speed = 3;
+		private uint _bumpCount = 0;
+		public event BallLostEventHandler BallLosted;
 
 		public Ball ()
 		{
@@ -19,6 +37,13 @@ namespace OneHourPong
 			_direction.Y = 1;
 		}
 
+		protected virtual void OnBallLosted(BallLostEventArgs e)
+		{
+			if (BallLosted != null) {
+				BallLosted (this, e);
+			}
+		}
+
 		public void Draw(RenderWindow window)
 		{
 			_ball.Position = _position;
@@ -27,14 +52,34 @@ namespace OneHourPong
 
 		public void Update()
 		{
-			_position.X += _direction.X * 3;
-			_position.Y += _direction.Y * 3;
+			_position.X += _direction.X * _speed;
+			_position.Y += _direction.Y * _speed;
+
+			if (_position.X < 45) {
+				OnBallLosted (new BallLostEventArgs (Paddle.Where.Right));
+				return;
+			} else if (_position.X > 770) {
+				OnBallLosted (new BallLostEventArgs (Paddle.Where.Left));
+				return;
+			}
+
 			if (_position.Y <= 0) {
 				_position.Y = 0;
+				_bumpCount++;
 				_direction.Y *= -1;
 			} else if (_position.Y >= 590) {
 				_position.Y = 590;
 				_direction.Y *= -1;
+				_bumpCount++;
+			}
+
+			if (_bumpCount > 10) {
+				_bumpCount = 0;
+			}
+
+			if (_bumpCount == 10) {
+				_speed += SpeedIncrease;
+				_bumpCount = 0;
 			}
 		}
 
@@ -50,15 +95,24 @@ namespace OneHourPong
 					if (paddle.Position.X + 10 >= _position.X) {
 						_direction.X = 1;
 						_position.X = paddle.Position.X + 10;
+						_bumpCount++;
 					}
 				} else if (paddle.ScreenSide == Paddle.Where.Right && _direction.X == 1) {
 					if (paddle.Position.X <= _position.X + 10) {
 						_direction.X = -1;
 						_position.X = paddle.Position.X;
+						_bumpCount++;
 					}
 				}
 			}
+		}
 
+		public void Reset()
+		{
+			_speed = 3;
+			_position.X = 400;
+			_position.Y = 300;
+			_ball.Position = _position;
 		}
 	}
 }
--- OneHourPong/Game.cs
+++ OneHourPong/Game.cs
@@ -1,10 +1,9 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: Regis
+ * One Hour Pong Game 
+ * (c) Régis FLORET 2024 and later
  * Date: 22/11/2014
  * Time: 18:09
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 
@@ -14,7 +13,7 @@ using SFML.Window;
 namespace OneHourPong
 {
 	/// <summary>
-	/// Description of Game.
+	/// This is the main game class. handle event and so on.
 	/// </summary>
 	public class Game
 	{
@@ -22,23 +21,85 @@ namespace OneHourPong
 		private Paddle leftPaddle;
 		private Paddle rightPaddle;
 		private Ball ball;
+		private Score _score;
+		private bool _pause = false;
+		private Text _pauseText;
+		private StartScene _startScene;
 
+		/// <summary>
+		/// Create SFML window, connect event, create game objects.
+		/// </summary>
 		public Game()
 		{
 			// Create the render window
 			window = new RenderWindow (new VideoMode (800, 600), "One hour pong");
 			window.SetFramerateLimit (60);
-			window.SetKeyRepeatEnabled(true);
+			window.SetKeyRepeatEnabled (true);
+			window.SetVerticalSyncEnabled (true);
 
 			// Connect event to methods
 			window.Closed += window_Closed;
+			window.KeyReleased += HandleKeyReleased;
 
 			// Create objects
 			leftPaddle = new Paddle (Paddle.Where.Left);
-			rightPaddle = new Paddle (Paddle.Where.Right);
+			rightPaddle = new Paddle (Paddle.Where.Right); 	 
+			_score = new Score ();
+
 			ball = new Ball ();
+			ball.BallLosted += HandleBallLosted;
+
+			_pauseText = new Text("Pause", new SFML.Graphics.Font ("Data/Square.ttf"), 100);
+			_pauseText.Position = new Vector2f (
+				(800 - _pauseText.GetLocalBounds ().Width) / 2f, 
+				(600 - _pauseText.GetLocalBounds ().Height) / 2f
+			);
+			Playing = false;
+			_startScene = new StartScene ();
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="OneHourPong.Game"/> is playing.
+		/// </summary>
+		/// <value><c>true</c> if playing; otherwise, <c>false</c>.</value>
+		public bool Playing {
+			get;set;
+		}
+
+		/// <summary>
+		/// Handles the ball losted.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		void HandleBallLosted (object sender, BallLostEventArgs e)
+		{
+			if (e.Side == Paddle.Where.Right) {
+				_score.UpdateLeft ();
+			} else if (e.Side == Paddle.Where.Left) {
+				_score.UpdateRight ();
+			} 
+			ball.Reset ();
+		}
+
+		/// <summary>
+		/// Handles the key released.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		void HandleKeyReleased (object sender, KeyEventArgs e)
+		{
+			if (!Playing) {
+				Playing = true;
+			} else if (e.Code == Keyboard.Key.P) {
+				_pause = !_pause;
+			} else if (e.Code == Keyboard.Key.Escape && Playing == true) {
+				Playing = false;
+			}
+		}
+
+		/// <summary>
+		/// Run the game
+		/// </summary>
 		public void Run() 
 		{
 			while (window.IsOpen())
@@ -46,20 +107,36 @@ namespace OneHourPong
 				window.DispatchEvents();
 				window.Clear();
 
-				leftPaddle.Update ();
-				rightPaddle.Update ();
-				ball.Collide (leftPaddle);
-				ball.Collide (rightPaddle);
-				ball.Update ();
+				if (Playing) {
+					// Do game stuff
+					if (!_pause) {
+						leftPaddle.Update ();
+						rightPaddle.Update ();
+						ball.Collide (leftPaddle);
+						ball.Collide (rightPaddle);
+						ball.Update ();
+					} else {
+						window.Draw (_pauseText);
+					}
 
-				leftPaddle.Draw (window);
-				rightPaddle.Draw (window);
-				ball.Draw (window);
+					_score.Draw (window);		
+					leftPaddle.Draw (window);
+					rightPaddle.Draw (window);
+					ball.Draw (window);
+				} else {
+					// Display start panel
+					_startScene.Draw (window);
+				}
 
 				window.Display();
 			}
 		}
 
+		/// <summary>
+		/// Close the window. This will ends the game.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
 		void window_Closed(object sender, EventArgs e)
 		{
 			window.Close();
